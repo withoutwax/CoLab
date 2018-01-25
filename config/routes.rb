c149b752d0ee4646eed83a5cb03470058b25aba8
Will Kim <rlagmlckd@gmail.com>
2018-01-25 00:38:17 -0600

    Setting up Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'co_labs/index'
+
+  devise_for :users
+
+  root to: "co_labs#index"
 end
